c2890e2237db09391c6ade3934b7f1897188794b
Alan Johnson <me@alanjohnson.io>
2026-02-13 15:19:46 -0600

    updated .js files
    add copywrite

--- src/providers/google/google.js
+++ src/providers/google/google.js
@@ -1,3 +1,9 @@
+/**
+ * Handsbreadth Task Server
+ * Copyright (c) 2026 Handsbreadth Software LLC.
+ * All rights reserved.
+ */
+
 const { google } = require('googleapis');
 
 class GoogleTasksProvider {
